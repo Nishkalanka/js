f862d13d9d0bae7a99d517983c3bc287cc977aa4
Павел Исаев <nishkalanka@MacBook-Pro-Pavel.local>
2020-05-22 10:56:19 +0530

    commit

--- src/main.js
+++ src/main.js
@@ -1,26 +1,26 @@
-var prev = document.querySelector("#gallery .prev");
 var next = document.querySelector("#gallery .next");
+var prev = document.querySelector("#gallery .prev");
 var sliders = document.querySelectorAll("#gallery ul li");
 var i = 0;
 
 next.onclick = function () {
-  sliders[i].style.display = "none";
+  sliders[i].className = "";
   i = i + 1;
 
   if (i >= sliders.length) {
     i = 0;
   }
 
-  sliders[i].style.display = "block";
+  sliders[i].className = "active";
 };
 
 prev.onclick = function () {
-  sliders[i].style.display = "none";
+  sliders[i].className = "";
+
   i = i - 1;
 
   if (i < 0) {
     i = sliders.length - 1;
   }
-
-  sliders[i].style.display = "block";
+  sliders[i].className = "active";
 };
